3ff16c02e9f5783718ce81e237c86bd95cc1b068
H0mire <janikmueller12@gmail.com>
2023-07-11 12:40:29 +0200

    Added comments

--- ExerciseSample/app/src/main/java/com/example/exercise/ExerciseFragment.kt
+++ ExerciseSample/app/src/main/java/com/example/exercise/ExerciseFragment.kt
@@ -176,103 +176,186 @@ class ExerciseFragment : Fragment() {
 
         }
     }
-
+    // Ableiten der restlichen Variablen für den Payload
     private fun derivePayload(){
+        //Dauer entspricht ca. aufnahmezeitpunkt des letzen Messpunktes.
         var durationInSeconds: Long = this.pulseData.last().time.toLong();
+        // Aktuelles Datum ermitteln
         val currentDate = LocalDate.now()
+
+        // Formatter für das Datumsformat "dd.MM.yyyy" erstellen
         val formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy")
+
+        // Formatieren des aktuellen Datums mit dem Formatter
         val formattedDate = currentDate.format(formatter)
 
+        // Erstellen eines Data-Objekts mit den relevanten Daten
+        val data = Data(
+            formattedDate,
+            durationInSeconds,
+            avgPulse,
+            maxPulse,
+            minPulse,
+            avgSpeed,
+            sumSteps,
+            pulseData,
+            speedData
+        )
 
-        val data = Data(formattedDate, durationInSeconds,avgPulse, maxPulse, minPulse, avgSpeed, sumSteps, pulseData, speedData)
+        // Erzeugen einer Gson-Instanz zum Konvertieren des Data-Objekts in JSON
         val gson = Gson()
+
+        // Konvertieren des Data-Objekts in ein JSON-Payload
         val jsonPayload = gson.toJson(data)
+
         Log.d(TAG, "6")
 
+        // Starten eines asynchronen Tasks mit GlobalScope.launch
         GlobalScope.launch {
             try {
+                // Durchführen des Logins, um das JWT-Token zu erhalten
                 login()
-                sendData( jsonPayload)
+
+                // Senden des JSON-Payloads über eine HTTP-POST-Anfrage
+                sendData(jsonPayload)
+
                 println("HTTP POST Request erfolgreich gesendet")
             } catch (e: Exception) {
                 println("Fehler beim Senden des HTTP POST Requests: ${e.message}")
             }
         }
-
     }
+
     data class DataPoint(val time: Double, val value: Double)
 
     data class Data(val date: String, val durationInSeconds: Long, val avgPulse: Int, val maxPulse: Int, val minPulse: Int, val avgSpeed: Long, val sumSteps: Int, val pulseData: List<DataPoint>, val speedData: List<DataPoint>)
     data class User(val username: String, val password: String)
 
     data class TokenContainer(val token:String)
+
+    //Login-Funktion für die Authentifizierung und Erhalt eines JWT-Tokens
     private fun login(){
         var username="admin";
         var password="hashed";
+        // Erzeugen einer Gson-Instanz zum Konvertieren von Objekten in JSON
         val gson = Gson()
-        val jsonPayload = gson.toJson(User(username, password));
+
+        // Erzeugen des JSON-Payloads aus Benutzername und Passwort
+        val jsonPayload = gson.toJson(User(username, password))
+
+        // Erzeugen einer URL-Instanz mit der Login-Endpunktadresse
         val url = URL("http://janikmichael.ddns.net:3001/auth/login")
+
+        // Öffnen einer Verbindung zur angegebenen URL
         val connection = url.openConnection() as HttpURLConnection
+
+        // Festlegen der Anfragemethode auf POST
         connection.requestMethod = "POST"
+
+        // Festlegen des Content-Type-Headers auf "application/json; charset=UTF-8"
         connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8")
+
+        // Aktivieren des Output-Modus, um Daten zu senden
         connection.doOutput = true
 
         try {
+            // Erzeugen eines OutputStreams, um Daten zu schreiben
             val outputStream: OutputStream = BufferedOutputStream(connection.outputStream)
+
+            // Schreiben des JSON-Payloads in den OutputStream
             outputStream.write(jsonPayload.toByteArray(Charsets.UTF_8))
+
+            // Flushen des OutputStreams, um sicherzustellen, dass alle Daten gesendet werden
             outputStream.flush()
 
+            // Abrufen des HTTP-Antwortcodes
             val responseCode = connection.responseCode
+
+            // Überprüfen, ob die Anfrage erfolgreich war (HTTP-Statuscode 200)
             if (responseCode == HttpURLConnection.HTTP_OK) {
-                val response = connection.inputStream.bufferedReader().use { it.readText() } // bitte hier token handeln
+                // Lesen der Antwortdaten als String
+                val response = connection.inputStream.bufferedReader().use { it.readText() }
 
+                // Konvertieren der JSON-Antwort in ein TokenContainer-Objekt
                 val jsonResponse = gson.fromJson(response, TokenContainer::class.java)
-                val token = jsonResponse.token
-                this.accessTokenJWT = token;
 
+                // Extrahieren des JWT-Tokens aus dem TokenContainer
+                val token = jsonResponse.token
+
+                // Setzen des JWT-Tokens als Zugriffstoken
+                this.accessTokenJWT = token
+
+                // Ausgeben der Antwort
                 println("Antwort: $response")
             } else {
                 // Fehler bei der Anfrage
                 val responseMessage = connection.responseMessage
+
+                // Ausgeben der Fehlermeldung
                 println("Fehler bei der Anfrage: $responseMessage")
             }
         } catch (e: Exception) {
             // Fehler bei der Verbindung oder der Datenübertragung
             println("Fehler bei der Verbindung oder Datenübertragung: ${e.message}")
         } finally {
+            // Trennen der Verbindung
             connection.disconnect()
         }
-
     }
+    //Senden der Gesundheits und Trainingsdaten an das Backend
     private fun sendData(jsonPayload: String) {
         val url = URL("http://janikmichael.ddns.net:3001/app/trainings")
+        // Öffnen einer Verbindung zur angegebenen URL
         val connection = url.openConnection() as HttpURLConnection
+
+        // Festlegen der Anfragemethode auf POST
         connection.requestMethod = "POST"
+
+        // Festlegen des Content-Type-Headers auf "application/json; charset=UTF-8"
         connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8")
-        connection.setRequestProperty("Authorization", "Bearer "+this.accessTokenJWT)
+
+        // Festlegen des Authorization-Headers mit dem JWT-Token
+        connection.setRequestProperty("Authorization", "Bearer " + this.accessTokenJWT)
+
+        // Aktivieren des Output-Modus, um Daten zu senden
         connection.doOutput = true
 
         try {
+            // Erzeugen eines OutputStreams, um Daten zu schreiben
             val outputStream: OutputStream = BufferedOutputStream(connection.outputStream)
+
+            // Konvertieren des JSON-Payloads in ein Byte-Array mit UTF-8-Encoding
             outputStream.write(jsonPayload.toByteArray(Charsets.UTF_8))
+
+            // Flushen des OutputStreams, um sicherzustellen, dass alle Daten gesendet werden
             outputStream.flush()
 
+            // Abrufen des HTTP-Antwortcodes
             val responseCode = connection.responseCode
+
+            // Überprüfen, ob die Anfrage erfolgreich war (HTTP-Statuscode 200)
             if (responseCode == HttpURLConnection.HTTP_OK) {
+                // Lesen der Antwortdaten als String
                 val response = connection.inputStream.bufferedReader().use { it.readText() }
+
+                // Ausgeben der Antwort
                 println("Antwort: $response")
             } else {
                 // Fehler bei der Anfrage
                 val responseMessage = connection.responseMessage
+
+                // Ausgeben der Fehlermeldung
                 println("Fehler bei der Anfrage: $responseMessage")
             }
         } catch (e: Exception) {
             // Fehler bei der Verbindung oder der Datenübertragung
             println("Fehler bei der Verbindung oder Datenübertragung: ${e.message}")
         } finally {
+            // Trennen der Verbindung
             connection.disconnect()
         }
     }
+
 
     private fun tryStartExercise() {
         viewLifecycleOwner.lifecycleScope.launch {
@@ -359,59 +442,86 @@ class ExerciseFragment : Fragment() {
         binding.pauseResumeButton.setText(if (state.isPaused) R.string.resume else R.string.pause)
         binding.pauseResumeButton.isEnabled = !state.isEnded
     }
-
+    //Metriken aktualisieren basierend auf den neuesten Datenpunkten
     private fun updateMetrics(latestMetrics: DataPointContainer) {
         this.latestMetrics = latestMetrics;
+        // Aktualisieren der Herzfrequenzdaten
         latestMetrics.getData(DataType.HEART_RATE_BPM).let {
             if (it.isNotEmpty()) {
                 binding.heartRateText.text = it.last().value.roundToInt().toString()
-                if(this.timeSinceBootInMillis == 0L){
-                    this.timeSinceBootInMillis = it.last().timeDurationFromBoot.toMillis();
+
+                // Überprüfen, ob die Zeit seit dem Start des Geräts bereits erfasst wurde
+                if (this.timeSinceBootInMillis == 0L) {
+                    this.timeSinceBootInMillis = it.last().timeDurationFromBoot.toMillis()
                 }
-                var time = (it.last().timeDurationFromBoot.toMillis()-timeSinceBootInMillis)/1000.0;
-                if(this.pulseData.isEmpty() || time-this.pulseData.last().time>0.5)
-                    this.pulseData.add(DataPoint(time, it.last().value));
-                else
-                    this.pulseData.add(DataPoint(time+1, it.last().value));
+
+                // Berechnen der vergangenen Zeit seit dem Start des Geräts in Sekunden
+                var time = (it.last().timeDurationFromBoot.toMillis() - timeSinceBootInMillis) / 1000.0
+
+                // Überprüfen, ob neue Datenpunkte hinzugefügt werden sollen
+                if (this.pulseData.isEmpty() || time - this.pulseData.last().time > 0.5) {
+                    this.pulseData.add(DataPoint(time, it.last().value))
+                } else {
+                    this.pulseData.add(DataPoint(time + 1, it.last().value))
+                }
             }
         }
+
+        // Aktualisieren der Schrittgeschwindigkeitsdaten
         latestMetrics.getData(DataType.STEPS_PER_MINUTE).let {
             if (it.isNotEmpty()) {
-                if(this.timeSinceBootInMillis == 0L){
-                    this.timeSinceBootInMillis = it.last().timeDurationFromBoot.toMillis();
+                // Überprüfen, ob die Zeit seit dem Start des Geräts bereits erfasst wurde
+                if (this.timeSinceBootInMillis == 0L) {
+                    this.timeSinceBootInMillis = it.last().timeDurationFromBoot.toMillis()
                 }
-                var time = (it.last().timeDurationFromBoot.toMillis()-timeSinceBootInMillis)/1000.0;
-                if(this.speedData.isEmpty() || time-this.speedData.last().time>0.5)
-                    this.speedData.add(DataPoint(time, it.last().value.toDouble()));
-                else
-                    this.speedData.add(DataPoint(time+1, it.last().value.toDouble()));
+
+                // Berechnen der vergangenen Zeit seit dem Start des Geräts in Sekunden
+                var time = (it.last().timeDurationFromBoot.toMillis() - timeSinceBootInMillis) / 1000.0
+
+                // Überprüfen, ob neue Datenpunkte hinzugefügt werden sollen
+                if (this.speedData.isEmpty() || time - this.speedData.last().time > 0.5) {
+                    this.speedData.add(DataPoint(time, it.last().value.toDouble()))
+                } else {
+                    this.speedData.add(DataPoint(time + 1, it.last().value.toDouble()))
+                }
             }
         }
+
+        // Aktualisieren des Durchschnittsgeschwindigkeitswerts
         latestMetrics.getData(DataType.STEPS_PER_MINUTE_STATS).let {
-            if (it!=null) {
+            if (it != null) {
                 avgSpeed = it.average
             }
         }
+
+        // Aktualisieren der Herzfrequenzstatistikwerte
         latestMetrics.getData(DataType.HEART_RATE_BPM_STATS).let {
-            if (it!=null) {
-                avgPulse= it.average.toInt();
-                maxPulse = it.max.toInt();
-                minPulse = it.min.toInt();
+            if (it != null) {
+                avgPulse = it.average.toInt()
+                maxPulse = it.max.toInt()
+                minPulse = it.min.toInt()
             }
         }
+
+        // Aktualisieren der Gesamtzahl der Schritte
         latestMetrics.getData(DataType.STEPS_TOTAL).let {
-            if (it!=null) {
-                sumSteps = it.total.toInt();
+            if (it != null) {
+                sumSteps = it.total.toInt()
             }
         }
+
+        // Aktualisieren der Gesamtstrecke
         latestMetrics.getData(DataType.DISTANCE_TOTAL)?.let {
             binding.distanceText.text = formatDistanceKm(it.total)
         }
+
+        // Aktualisieren der Gesamtverbrannten Kalorien
         latestMetrics.getData(DataType.CALORIES_TOTAL)?.let {
             binding.caloriesText.text = formatCalories(it.total)
         }
-
     }
+
+
 
     private fun updateLaps(laps: Int) {
         binding.lapsText.text = laps.toString()
